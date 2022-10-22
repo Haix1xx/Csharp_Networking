763a7fe72a6edc229bedc856066bffe334022907
TruongHoangNhatHuy <102200094@sv1.dut.udn.vn>
2022-10-22 18:02:16 +0700

    Complete Test project

--- Test/Download.cs
+++ Test/Download.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +12,139 @@ namespace Test
 {
     internal class Download
     {
+        private string url;
+        private string filePath;
+        private List<Range> ranges;
+        // Dictionary chứa các file nhớ tạm
+        private ConcurrentDictionary<int, string> dict;
+        public Download(string url, string filePath, List<Range> ranges)
+        {
+            this.url = url;
+            this.filePath = filePath;
+            this.ranges = ranges;
+            dict = new ConcurrentDictionary<int, string>();
+        }
+
+        public async Task SingleStreamDownload(string url, string fileName)
+        {
+            var httpClient = new HttpClient();
+            try
+            {
+                // Gửi HTTP request, chờ phản hồi từ URL
+                var responseMessage = await httpClient.GetAsync(url);
+                // Đọc nội dung của HTTP response
+                var stream = await responseMessage.Content.ReadAsStreamAsync();
+                // Biến bộ nhớ đệm, khi đọc đủ kích thước bộ nhớ đệm thì sẽ ghi vào file, rồi tiếp tục đọc
+                const int SIZEBUFFER = 500;
+                var buffer = new byte[SIZEBUFFER];
+                //
+                int numberByte = 0;
+                // Xác định file ghi
+                var streamwrite = File.OpenWrite(fileName);
+                // Vòng lặp đọc & ghi
+                do
+                {
+                    // Đọc nội dung vào bộ nhớ đệm
+                    numberByte = await stream.ReadAsync(buffer, 0, SIZEBUFFER);
+                    // Ghi dữ liệu bộ nhớ đệm vào file
+                    await streamwrite.WriteAsync(buffer, 0, numberByte);
+                    Console.WriteLine("Downloading");
+                }
+                while (numberByte > 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+        public async Task ParallelDownload()
+        { 
+            try
+            {
+                // List các thread
+                List<Task> allTask = new List<Task>();
+                // Với mỗi thread, tải 1 range của file
+                foreach (Range range in ranges)
+                {
+                    // Thêm thread vào list
+                    allTask.Add(PartialDownload(url, range));
+                }
+                // Chờ mọi thread tải xong
+                await Task.WhenAll(allTask);
+                // Tạo file đích
+                FileStream fileStream = new FileStream(filePath, FileMode.Append);
+                // Vòng lặp ghép file
+                foreach (var tempFile in dict.OrderBy(a => a.Key))
+                {
+                    // Chỉ ghép 1 file tại 1 thời điểm
+                    lock (fileStream)
+                    {
+                        Console.WriteLine($"Key: {tempFile.Key}, Path: {tempFile.Value}");
+                        // Đọc file nhớ tạm
+                        byte[] tempFileByte = File.ReadAllBytes(tempFile.Value);
+                        // Ghi vào file đích
+                        fileStream.Write(tempFileByte, 0, tempFileByte.Length);
+                        // Xóa file nhớ tạm
+                        File.Delete(tempFile.Value);
+                    }
+                }
+                fileStream.Close();
+                Console.WriteLine("Parallel download done");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        // Hàm tải từng phần của 1 file
+        private async Task PartialDownload(string url, Range range)
+        {
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage();
+            request.Method = HttpMethod.Get;
+            request.RequestUri = new Uri(url);
+            // Xác định phần nội dung cần tải, từ vị trí start đến vị trí end
+            if (request.Headers.Contains("Range"))
+                request.Headers.Remove("Range");
+            request.Headers.Add("Range", $"bytes={range.Start}-{range.End}");
+            // Gửi HTTP request, chờ phản hồi từ URL
+            var responseMessage = await httpClient.SendAsync(request);
+            // Đọc nội dung của HTTP response
+            var stream = await responseMessage.Content.ReadAsStreamAsync();
+            // Ghi nội dung vào 1 file nhớ tạm
+            string tempFilePath = await createTempFile(stream);
+            Console.WriteLine(tempFilePath);
+            // Gắn ChunkIndex & tempFilePath vào 1 Dictionary
+            dict.TryAdd(range.ChunkIndex, tempFilePath);
+            // Giải phóng
+            stream.Close();
+            request.Dispose();
+            httpClient.Dispose();
+        }
+        private static async Task<string> createTempFile(Stream data)
+        {
+            // Tạo file nhớ tạm
+            var tempFilePath = Path.GetTempFileName();
+            // Biến bộ nhớ đệm, khi đọc đủ kích thước bộ nhớ đệm thì sẽ ghi vào file, rồi tiếp tục đọc
+            const int SIZEBUFFER = 1024;
+            var buffer = new byte[SIZEBUFFER];
+            // Biến đếm byte đã đọc
+            int numberByteRead = 0;
+            // Xác định file ghi
+            var streamWrite = File.OpenWrite(tempFilePath);
+            // Vòng lặp đọc & ghi
+            do
+            {
+                // Đọc nội dung vào bộ nhớ đệm
+                numberByteRead = await data.ReadAsync(buffer, 0, SIZEBUFFER);
+                // Ghi dữ liệu bộ nhớ đệm vào file
+                await streamWrite.WriteAsync(buffer, 0, numberByteRead);
+                // to do: progress counter code
+            }
+            while (numberByteRead > 0);
+            streamWrite.Close();
+            return tempFilePath;
+        }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -12,20 +12,9 @@ namespace Test
         {
             string url = "http://sv.dut.udn.vn/G_ListCTDT.aspx";
             string url1 = "https://raw.githubusercontent.com/xuanthulabnet/learn-cs-netcore/master/imgs/cs022.png";
-            //var result = await Client.GetFileLength(url);
-            //Console.WriteLine(result.ToString());
-
-            //var task1 = new Task(() => Client.DownloadStream(url, "file1.aspx"));
-            //var task2 = new Task(() => Client.DownloadStream(url, "file2.aspx"));
-            //var task3 = new Task(() => Client.DownloadStream(url, "file3.aspx"));
-            //task1.Start();
-            //task2.Start();
-            //task3.Start();
-            //await task1;
-            //await task2;
-            //await task3;
-            var task = await Client.GetFileLength(url1);
-            Console.WriteLine(task);
+            // Chỉ rõ url, filePath & số thread
+            await Client.BeginDownload(url1, "C:\\Users\\PC\\Desktop\\abc\\xyz.png", 4);
+            Console.WriteLine("done");
             Console.ReadKey();
         }
 
